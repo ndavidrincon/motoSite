e0cf32956cd69d9a9aec3a917849cc2ed2509d74
ndavidrincon <digitalonly.agency@gmail.com>
2025-12-09 13:54:53 -0500

    add code of add_product.html and modify view

--- ventasGuerrero/views.py
+++ ventasGuerrero/views.py
@@ -1,6 +1,13 @@
+from django.urls import reverse_lazy
 from django.views import generic
 from .forms import VentasGuerreroForm
 
 class VentasGuerreroFormView(generic.FormView):
     template_name = "products/add_product.html"
     form_class = VentasGuerreroForm
+    success_url = reverse_lazy("add_product")
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+    
