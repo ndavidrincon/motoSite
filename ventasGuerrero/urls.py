91dd64f6b33ca21621627abb3f6e914acfc779ab
ndavidrincon <digitalonly.agency@gmail.com>
2025-12-09 15:19:15 -0500

    Add list_prodcts.html and its view

--- ventasGuerrero/urls.py
+++ ventasGuerrero/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import VentasGuerreroFormView
+from .views import ProductListView, VentasGuerreroFormView
 
 urlpatterns = [
+    path('', ProductListView.as_view(), name="list_product"),
     path('agregar/', VentasGuerreroFormView.as_view(), name="add_product"),
 ]
--- ventasGuerrero/views.py
+++ ventasGuerrero/views.py
@@ -1,5 +1,7 @@
 from django.urls import reverse_lazy
 from django.views import generic
+
+from ventasGuerrero.models import ProductGuerrero
 from .forms import VentasGuerreroForm
 
 class VentasGuerreroFormView(generic.FormView):
@@ -10,4 +12,9 @@ class VentasGuerreroFormView(generic.FormView):
     def form_valid(self, form):
         form.save()
         return super().form_valid(form)
+    
+class ProductListView(generic.ListView):
+    model = ProductGuerrero
+    template_name = 'products/list_products.html'
+    context_object_name = 'products'
     
