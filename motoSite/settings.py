cc8d331f8dfb82a063fc05cf1cdda6bea75632dd
ndavidrincon <digitalonly.agency@gmail.com>
2025-12-11 23:12:49 -0500

    Add register.html to users.template and modify all configurations

--- motoSite/settings.py
+++ motoSite/settings.py
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-co'
 
 TIME_ZONE = 'UTC'
 
@@ -129,4 +129,5 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
 CRISPY_TEMPLATE_PACK = "tailwind"
 
 LOGIN_REDIRECT_URL = "list_product"
+LOGOUT_REDIRECT_URL = "list_product"
 
