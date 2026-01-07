442bced7d5430052ec1ae93c583ec490737c9981
ndavidrincon <digitalonly.agency@gmail.com>
2026-01-06 22:40:23 -0500

    Add configuration of the users.admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+
+    def has_delete_permission(self, request, obj=None):
+        # Bloquear eliminación del usuario ndavidrincon
+        if obj and obj.is_superuser:
+            return False
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj and obj.is_superuser:
+            return (
+                "password",
+                "is_active",
+                "is_staff",
+                "is_superuser",
+                "groups",
+                "user_permissions",
+            )
+        return super().get_readonly_fields(request, obj)
+
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
