c22fcd7f31eacf4d5945e85c3edd6035862a0896
ndavidrincon <digitalonly.agency@gmail.com>
2026-01-06 20:20:43 -0500

    home styles and about ready

--- motoSite/settings.py
+++ motoSite/settings.py
@@ -18,6 +18,7 @@ import environ
 BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
+# print("DJANGO_DB_URL:", env("DJANGO_DB_URL", default="NO CARGADA"))
 
 
 # Quick-start development settings - unsuitable for production
